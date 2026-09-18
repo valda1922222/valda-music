aee618fcd96e266be290ce020ccb7eb662cb9802
valda1922222 <yusufhabibi888666@gmail.com>
2026-09-19 05:02:04 +0700

    Add Song data class for music representation

---
+++ app/src/main/java/com/valda/music/Song.kt
@@ -0,0 +1,11 @@
+package com.valda.music
+
+data class Song(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val durationMs: Long,
+    val uri: String,
+    val albumArtUri: String?
+)
